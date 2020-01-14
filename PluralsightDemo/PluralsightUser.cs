a970aa2fc81759b74763460c13f713dcc2c676fb
Joao Filipe <Romao>
2020-01-14 17:33:33 +0000

    first commit

---
+++ PluralsightDemo/PluralsightUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PluralsightDemo
+{
+
+
+    public class PluralsightUser
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string NormalizedUsername { get; set; }
+        public string PasswordHash { get; set; }
+
+
+    }
+}
---
+++ PluralsightDemo/PluralsightUserStote.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PluralsightDemo
+{
+    class PluralsightUserStote : IUserStore<PluralsightUser>
+    { public void Dispose()
+        {
+          
+        }
+
+
+        public Task<string> GetUserIdAsync(PluralsightUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.Id);
+        }
+        public Task<IdentityResult> CreateAsync(PluralsightUser user, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IdentityResult> DeleteAsync(PluralsightUser user, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+       
+
+        public Task<PluralsightUser> FindByIdAsync(string userId, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<PluralsightUser> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<string> GetNormalizedUserNameAsync(PluralsightUser user, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+      
+
+        public Task<string> GetUserNameAsync(PluralsightUser user, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task SetNormalizedUserNameAsync(PluralsightUser user, string normalizedName, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task SetUserNameAsync(PluralsightUser user, string userName, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IdentityResult> UpdateAsync(PluralsightUser user, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
