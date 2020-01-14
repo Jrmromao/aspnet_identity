fd1e786e83ed6b93583bc7002761a7057b5ee9f4
Joao Filipe Romao <joao.romao@wipro.com>
2020-01-14 23:18:08 +0000

    topic 3 underway

--- PluralsightDemo/PluralsightUserStote.cs
+++ PluralsightDemo/PluralsightUserStote.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.Data.Common;
+using System.Data.SqlClient;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -7,25 +9,50 @@ namespace PluralsightDemo
     class PluralsightUserStote : IUserStore<PluralsightUser>
     { public void Dispose()
         {
-          
+
         }
-
-
         public Task<string> GetUserIdAsync(PluralsightUser user, CancellationToken cancellationToken)
         {
             return Task.FromResult(user.Id);
         }
-        public Task<IdentityResult> CreateAsync(PluralsightUser user, CancellationToken cancellationToken)
+
+        public Task<string> GetUserNameAsync(PluralsightUser user, CancellationToken cancellationToken)
         {
-            throw new System.NotImplementedException();
+            return Task.FromResult(user.Username);
+        }
+        public Task SetUserNameAsync(PluralsightUser user, string userName, CancellationToken cancellationToken)
+        {
+            user.Username = userName;
+            return Task.CompletedTask;
+        }
+        public Task SetNormalizedUserNameAsync(PluralsightUser user, string normalizedName, CancellationToken cancellationToken)
+        {
+            user.NormalizedUsername = normalizedName;
+            return Task.CompletedTask;
+        }
+        public static DbConnection GetOpenConnection()
+        {
+            var connection = new SqlConnection("Data Source=(localDb)\\MSSQLLocalDB;" +
+                "database=PluralsightDemo;" +
+                "truested_connection=yes;");
+            return connection;
+        }
+
+
+        public async Task<IdentityResult> CreateAsync(PluralsightUser user, CancellationToken cancellationToken)
+        {
+
+            using(var connection = GetOpenConnection())
+            {
+                await connection.Excecu
+            }
+            
         }
 
         public Task<IdentityResult> DeleteAsync(PluralsightUser user, CancellationToken cancellationToken)
         {
             throw new System.NotImplementedException();
         }
-
-       
 
         public Task<PluralsightUser> FindByIdAsync(string userId, CancellationToken cancellationToken)
         {
@@ -42,23 +69,10 @@ namespace PluralsightDemo
             throw new System.NotImplementedException();
         }
 
+   
+
       
-
-        public Task<string> GetUserNameAsync(PluralsightUser user, CancellationToken cancellationToken)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Task SetNormalizedUserNameAsync(PluralsightUser user, string normalizedName, CancellationToken cancellationToken)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Task SetUserNameAsync(PluralsightUser user, string userName, CancellationToken cancellationToken)
-        {
-            throw new System.NotImplementedException();
-        }
-
+      
         public Task<IdentityResult> UpdateAsync(PluralsightUser user, CancellationToken cancellationToken)
         {
             throw new System.NotImplementedException();
